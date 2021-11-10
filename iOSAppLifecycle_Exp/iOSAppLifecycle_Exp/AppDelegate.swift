bc303da91b7826ed95b200bb73bcb438a76348ab
Shak <shakfeizi@gmail.com>
2021-11-09 18:24:34 -0700

    Some Changes

--- iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/AppDelegate.swift
+++ iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/AppDelegate.swift
@@ -11,21 +11,10 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     // SHAK: Properties
-    var window: UIWindow?
-    var state: State! {
-        didSet {
-            self.store(state: self.state)
-            (self.window?.rootViewController as? ViewController)?.state = state
-        }
-    }
     
     // SHAK: Functions
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        self.window = UIWindow()
-        self.window?.rootViewController = ViewController(state: self.state)
-        self.window?.rootViewController?.view.backgroundColor = .purple
-        self.window?.makeKeyAndVisible()
-        self.updateState(with: application, event: .appDidFinishLaunching)
+        
         return true
     }
 
@@ -39,55 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        self.state = self.retrieveState()
-            let event = Event(with: application, event: .appWillFinishLaunching)
-            self.state = self.state.byAppending(event: event)
-            return true
-    }
-    
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.updateState(with: application, event: .appWillTerminate)
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        self.updateState(with: application, event: .appDidBecomeActive)
-    }
-    
-    func applicationWillResignActive(_ application: UIApplication) {
-        self.updateState(with: application, event: .appWillResignActive)
-    }
-    
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        self.updateState(with: application, event: .appDidEnterBackground)
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        self.updateState(with: application, event: .appWillEnterForeground)
-    }
 }
 
-extension AppDelegate {
-    // SHAK: Properties
-    static let stateKey = "persistent_state_key"
-    
-    // SHAK: Functions
-    func retrieveState() -> State {
-        let storedState = UserDefaults.standard.data(forKey: Self.stateKey).flatMap {
-            try? JSONDecoder().decode(State.self, from: $0)
-        }
-        guard let state = storedState else { return State() }
-        return state
-    }
-    
-    func store(state: State) {
-        guard let stateData = try? JSONEncoder().encode(state) else { return }
-        UserDefaults.standard.setValue(stateData, forKey: Self.stateKey)
-    }
-    
-    fileprivate func updateState(with application: UIApplication, event kind: Event.Kind) {
-        let event = Event(with: application, event: kind)
-        self.state = self.state.byAppending(event: event)
-      }
-}
 
--- iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/ViewController.swift
+++ iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/ViewController.swift
@@ -11,16 +11,6 @@ class ViewController: UITableViewController {
     // SHAK: Properties
 //    var names = ["Sam", "Nima", "James", "Rihanna"]
 //    var cellID = "cell"
-    var state: State?
-    
-    init(state: State?) {
-        self.state = state
-    }
-    
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
