276d5c66d4407f8f738b332867aae987320df9a1
Shak <shakfeizi@gmail.com>
2021-11-12 20:37:08 -0700

    Update

--- iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/SceneDelegate.swift
+++ iOSAppLifecycle_Exp/iOSAppLifecycle_Exp/SceneDelegate.swift
@@ -11,10 +11,21 @@ import UserNotifications
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // SHAK: Properties
     var window: UIWindow?
+    var defaults: UserDefaults?
 
     // SHAK: Functions
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        if defaults?.string(forKey: "TerminationKey") != nil && connectionOptions.notificationResponse != nil {
+            let rootViewController = UINavigationController(rootViewController: ViewController())
+            window?.rootViewController =  rootViewController
+            window?.makeKeyAndVisible()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                NotificationCenter.default.post(name: NSNotification.Name ?? "", object: nil, userInfo: connectionOptions.notificationResponse?.notification.request.content.userInfo)
+            }
+        }
 //        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
 //        window?.windowScene = windowScene
 //        window?.makeKeyAndVisible()
